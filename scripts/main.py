7b0b67a8cd8256c68c8193f6a2e96a02c6ff8e11
Júlio César Nascimento Oliveira <jcno307@gmail.com>
2019-05-29 17:28:55 -0300

    Fixed some issues

--- scripts/main.py
+++ scripts/main.py
@@ -19,7 +19,7 @@ def main():
     parser.add_argument("--scale", dest="scale", required=True,
                         help="The scale of the image to resize it by")
     parser.add_argument("--output", dest="outputFile",
-                        required=False, help="The name/path of the output file + .txt")
+                        required=True, help="The name/path of the output file + .txt")
     parser.add_argument("--print", dest="printToConsole", required=False,
                         help="Decides if it should print the output to the console or not. False by default")
     parser.add_argument("--reverse", dest="reverse", required=False,
